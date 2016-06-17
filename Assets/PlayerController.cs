450f2e2bb78f677ced35e0e957490b8f4b79f0a1
Jma353 <jma353@cornell.edu>
2016-06-17 16:15:13 -0400

    Ball movement

---
+++ Assets/PlayerController.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerController : MonoBehaviour {
+
+	public float thrust; 
+	private Rigidbody rb; 
+
+	void Start () {
+		// Pull reference to the object's rigid body 
+		rb = GetComponent<Rigidbody>(); 
+	}
+
+	void FixedUpdate () {
+
+		// Pull inputs 
+		float moveHorizontal = Input.GetAxis("Horizontal"); 
+		float moveVertical = Input.GetAxis("Vertical"); 
+
+		// Compute the direction of movement 
+		Vector3 direction = new Vector3(moveHorizontal*thrust, 0.0f, moveVertical*thrust); 
+
+		// Apply this directional force to the rigid body
+		rb.AddForce(direction); 
+
+	}
+
+
+
+}
--- README.md
+++ README.md
@@ -1,3 +1,3 @@
 # Test Unity Repo 
 
-Repo set up to test the functionalities / merge workflow of a Unity3D project leveraging Unity's **Smart Merge** tool
+Repo set up to test the functionalities / merge workflow of a Unity3D project leveraging Unity's **Smart Merge** tool, as well as try out different Unity 3D functionalities.  
