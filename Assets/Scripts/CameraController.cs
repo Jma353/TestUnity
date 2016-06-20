0a2534bde8f37907311e48300727aa1a4d9217a1
Jma353 <jma353@cornell.edu>
2016-06-20 09:43:03 -0400

    Camera moves with ball

---
+++ Assets/Scripts/CameraController.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraController : MonoBehaviour {
+
+	// Refers to the player 
+	public GameObject player; 
+
+	// Initialized @ start 
+	private Vector3 offset; 
+
+	// Use this for initialization
+	void Start () {
+		// Grab the initial offset 
+		offset = this.transform.position - player.transform.position; 
+	}
+	
+	// LateUpdate to update this after everything is done 
+	void LateUpdate () {
+		// Reset the camera 
+		this.transform.position = player.transform.position + offset; 
+	}
+}
